e4f9a6bb9c3a1016f09a0e6f845226d9a8ddd1da
Jonathan Chien <jonathan.chien@gmail.com>
2015-05-27 18:30:27 -0700

    updated view controllers

--- ToDoList/AddToDoItemViewController.h
+++ ToDoList/AddToDoItemViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AddToDoItemViewController.h
 //  ToDoList
 //
 //  Created by FX Mobile 1 on 5/27/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface AddToDoItemViewController : UIViewController
 
 @end
-
